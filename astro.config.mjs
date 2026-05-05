fe04a7a7fceea3cdd12aa54ffa389fd3b0c274f7
Humberto Guerrero <lekobelekawame@gmail.com>
2026-05-04 18:47:52 -0600

    fix: remover sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://donutbitesco.com',
-  integrations: [sitemap()],
   compressHTML: true,
   build: {
     assets: '_assets',
